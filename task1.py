6da0670cc6bfb2e53910e60cf1caba4326b11a04
yilkimezakka@gmail.com <yilkimezakka@gmail.com>
2025-08-03 10:03:20 +0000

    first commit

---
+++ task1.py
@@ -0,0 +1,21 @@
+"""
+Task 1: Music Concert Ticketing System
+During the Jos Summer Music Concert, ticket sales were recorded as follows:
+
+tickets = {"VIP": 50, "Regular": 150, "Student": 75}
+
+Later in the day:
+- A new "Backstage" experience with 10 tickets was introduced.
+- The "Student" category sold out completely.
+- The team wanted to keep a record of the day’s sales before preparing for next week’s concert.
+"""
+
+tickets = {"VIP": 50, "Regular": 150, "Student": 75}
+Backstage = {"Backstage": 10}
+tickets.update(Backstage)
+print(tickets)
+tickets.pop("Student")
+print(tickets)
+tickets.clear
+print(tickets)
+

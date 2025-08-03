e667be8cbaac5a8c175c3e453d71cfc938c95a44
yilkimezakka@gmail.com <yilkimezakka@gmail.com>
2025-08-03 14:27:50 +0000

    task4.py commit

--- task4.py
+++ task4.py
@@ -10,5 +10,15 @@ During the first day:
 - The organizers created a record for the day before removing the most recently added participant from the live system.
 """
 
+participants = {"VIP": "Alice", "Regular": "Bob", "Student": "Charlie"}
+new_participants = {"Guest": "Daisy"}
+participants.update(new_participants)
+print(participants)
+participants.pop("Student")
+print(participants)
+participants.pop("Guest")
+print(participants)
+
+
 # print(participants_snapshot)
 # print(participants)
